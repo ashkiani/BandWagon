7d051ff1d92f5e181204aaecbec67b5352395140
Sia <ashkiani@yahoo.com>
2020-04-12 22:43:26 -0400

    Initial MySQL setup

---
+++ config/localConnection.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: "localhost",
+  port: "3306",
+  username: "enter your local MySQL username",
+  password: "enter your local MySQL password",
+};
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const Database = require("mysql-sync-query");
+const db = new Database("bandwagon_db");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -19,6 +21,29 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+try {
+  if (process.env.JAWSDB_URL) {
+    db.connectRemote(process.env.JAWSDB_URL);
+  } else {
+    let credential = require("./config/localConnection");
+    db.connectLocal(
+      credential.host,
+      credential.port,
+      credential.username,
+      credential.password
+    );
+  }
+  db.connection.connect(function (err) {
+    if (err) {
+      console.error("Failed to connect to DB."); //+ err.stack
+      return;
+    }
+    console.log("Connected to DB as id " + db.connection.threadId);
+    app.listen(PORT, async () => {
+      console.log(`🌎 ==> API server now on port ${PORT}!`);
+    });
+  });
+} catch (err) {
+  console.log("Failed to connect to DB.");
+  console.log(err);
+}
