d4abdc1ce9f7dd257cf50ddb9d817bff40f4eb2c
Sia <ashkiani@yahoo.com>
2020-04-13 20:30:56 -0400

    minor bug fix: redundant redirect

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -25,8 +25,7 @@ export default class Login extends Component {
     })
       .then(res => {
         if (res.status === 200) {
-          this.props.history.push("/");
-          window.location.assign("/searchconcert");
+          this.props.history.push("/searchconcert");
         } else {
           const error = new Error(res.error);
           throw error;
